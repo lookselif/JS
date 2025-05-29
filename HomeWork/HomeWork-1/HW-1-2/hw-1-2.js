109b965be7700d679cf1343f4328807b31f492a5
lookselif <lookselif@gmail.com>
2025-05-29 22:05:02 +0300

    done hw 1-js

---
+++ HomeWork/HomeWork-1/HW-1-2/hw-1-2.js
@@ -0,0 +1,5 @@
+let  firstName = 'Anton';
+let  middleName = ' Islamhanov';
+let  lastName = 'Andreevich';
+let person = firstName+middleName+' '+lastName
+console.log( person );
---
+++ HomeWork/HomeWork-1/HW-1-4/hw-1-4.js
@@ -0,0 +1,7 @@
+
+let name = prompt("Anton");
+let patronymic = prompt("Andreevich");
+let age = prompt("29");
+console.log("Anton",name);
+console.log("Andreevich",patronymic);
+console.log("29",age);
